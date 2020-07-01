7d1ac5d05f858be681438b0452ab938891375f71
Al Shakib <shakib@alshakib.dev>
2020-07-01 20:25:11 +0600

    Add an example

--- app/src/main/java/dev/alshakib/tasker/example/MainActivity.java
+++ app/src/main/java/dev/alshakib/tasker/example/MainActivity.java
@@ -21,13 +21,81 @@ package dev.alshakib.tasker.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import dev.alshakib.tasker.Tasker;
 
 public class MainActivity extends AppCompatActivity {
+    private final String LOG_TAG = this.getClass().getSimpleName();
+    private ImageView resultImageView;
+    private ProgressBar progressBar;
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultImageView = findViewById(R.id.result_image_view);
+        progressBar = findViewById(R.id.progress_bar);
+        resultTextView = findViewById(R.id.result_text_view);
+    }
+
+    public void onClickTryMe(View view) {
+        Tasker tasker = new Tasker();
+        tasker.executeAsync(new Tasker.Task<Bitmap>() {
+            @Override
+            protected void onPreExecute() {
+                // Show progress bar
+                progressBar.setVisibility(View.VISIBLE);
+                resultTextView.setVisibility(View.GONE);
+                resultImageView.setVisibility(View.GONE);
+            }
+
+            @Override
+            protected Bitmap doInBackground() {
+                // Download image in the background
+                Bitmap result = null;
+                try {
+                    URL ImageUrl = new URL(getResources().getString(R.string.image_url));
+                    HttpURLConnection conn = (HttpURLConnection) ImageUrl.openConnection();
+                    conn.setDoInput(true);
+                    conn.connect();
+                    InputStream inputStream = conn.getInputStream();
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inPreferredConfig = Bitmap.Config.RGB_565;
+                    result = BitmapFactory.decodeStream(inputStream, null, options);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap result) {
+                // Background job is completed.
+                // Set result bitmap to the image view
+                progressBar.setVisibility(View.GONE);
+                if (result != null) {
+                    resultImageView.setVisibility(View.VISIBLE);
+                    resultTextView.setVisibility(View.GONE);
+                    resultImageView.setImageBitmap(result);
+                } else {
+                    resultImageView.setVisibility(View.GONE);
+                    resultTextView.setVisibility(View.VISIBLE);
+                    resultTextView.setText(getResources().getString(R.string.result_error));
+                }
+            }
+        });
     }
 }
